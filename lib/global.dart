3330fb3da373e8b087206b3410dd67ad64039948
Diego Araujo <jcresgate@gmail.com>
2019-07-25 01:44:50 -0300

    add img path

--- lib/global.dart
+++ lib/global.dart
@@ -1,2 +1,5 @@
-const SERVER_NAME = "devsync.ddns.net";
-const CAT_GET_ALL = "http://$SERVER_NAME/api/Categoria/ListarTodos";
+const SERVER_API = "devsync.ddns.net";
+const SERVER_WEBSITE = "devsync.ddns.net";
+const TOKEN = "";
+const CAT_GET_ALL = "http://$SERVER_API/api/Categoria";
+const IMG_PATH = "$SERVER_WEBSITE/img/";
